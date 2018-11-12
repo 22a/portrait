6c6952d6851e21b206ae7dc8e1ce3b4f6ec5bd53
Peter Meehan <meehanp2@tcd.ie>
2018-11-12 01:25:14 +0000

    imlement b64 frame transport, lint + clean up comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-import 'babel-polyfill';
+import 'babel-polyfill'
 import 'phoenix_html'
-import uuid4 from "uuid4"
+import uuid4 from 'uuid4'
 
 import css from '../css/app.css'
 import channel from './socket'
@@ -9,12 +9,12 @@ channel.on('frame_resp', resp => {
   console.log({payloadIn: resp})
 })
 
+let userMediaStream
 let userMediaSettings = {
   video: {
     facingMode: 'environment'
   }
 }
-
 const videoElement = document.querySelector('#videoh')
 
 const switchPreferredUserMediaVideoSettings = () => {
@@ -27,56 +27,68 @@ const switchPreferredUserMediaVideoSettings = () => {
 }
 
 const sendFrameForProcessing = async () => {
-  const frameId = uuid4();
+  const frameId = uuid4()
   const frameContents = await grabFrameFromVideoStream()
+  const frameB64 = await blobToBase64(frameContents)
   const payload = {
     frameId,
-    frameContents,
+    frameContents: frameB64,
   }
-  console.log({payloadOut: payload})
   channel.push('frame_req', payload)
-    .receive("ok", resp => {
-      console.debug("frame request complete", resp)
+    .receive('ok', resp => {
+      console.debug('frame request complete', resp)
     })
-    .receive("error", resp => {
-      console.error("frame request error", resp)
+    .receive('error', resp => {
+      console.error('frame request error', resp)
     })
-    .receive("timeout", resp => {
-      console.error("frame request timeout", resp)
+    .receive('timeout', resp => {
+      console.error('frame request timeout', resp)
     })
 }
 
-let userMediaStream
+
+const blobToBase64 = async blob => {
+  return new Promise( resolve => {
+    const reader = new FileReader()
+    reader.onload = function() {
+        const dataUrl = reader.result
+        resolve(dataUrl)
+    }
+    reader.readAsDataURL(blob)
+  })
+}
 
 const grabFrameFromVideoStream = async () => {
   const track = userMediaStream.getVideoTracks()[0]
+  console.debug({ track })
   if (!track) {
-    console.log('grabFrameFromVideoStream, track does not exist')
+    console.error('grabFrameFromVideoStream, track does not exist')
     return null
   }
-  console.log({ track })
 
   if ('ImageCapture' in window) {
     const capture = new ImageCapture(track)
-    console.log({ capture })
+    console.debug({ capture })
     const frameBlob = await capture.takePhoto(userMediaSettings)
-    console.log({ frameBlob })
+    window.frameBlob = frameBlob
+    console.debug({ frameBlob })
     return frameBlob
   } else {
-    const canvas = document.createElement('canvas');
+    console.debug('ImageCapture not present on window, falling back to canvas')
+    const canvas = document.createElement('canvas')
     // TODO: pull these widths and heights from the stream / track
-    canvas.width = 640;
-    canvas.height = 480;
-    console.log({ canvas })
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-    console.log({ ctx })
+    canvas.width = 640
+    canvas.height = 480
+    console.debug({ canvas })
+    const ctx = canvas.getContext('2d')
+    ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height)
+    console.debug({ ctx })
     const frameBlob = await new Promise(resolve => {
       canvas.toBlob(blob => {
-        return resolve(blob);
-      });
-    });
-    console.log({ frameBlob })
+        return resolve(blob)
+      })
+    })
+    console.debug({ frameBlob })
     return frameBlob
   }
 }
--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,21 +1,21 @@
-import {Socket} from "phoenix"
-import uuid4 from "uuid4"
+import {Socket} from 'phoenix'
+import uuid4 from 'uuid4'
 
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+let socket = new Socket('/socket', {params: {token: window.userToken}})
 socket.connect()
 
-// "private" subtopic
+// 'private' subtopic
 let channel = socket.channel(`frames:${uuid4()}`, {})
 
 channel.join()
-  .receive("ok", resp => {
-    console.debug("successfully subscribed to frames channel", resp)
+  .receive('ok', resp => {
+    console.debug('successfully subscribed to frames channel', resp)
   })
-  .receive("error", resp => {
-    console.error("error subscribing to frames channel", resp)
+  .receive('error', resp => {
+    console.error('error subscribing to frames channel', resp)
   })
-  .receive("timeout", resp => {
-    console.error("timeout subscribing to frames channel", resp)
+  .receive('timeout', resp => {
+    console.error('timeout subscribing to frames channel', resp)
   })
 
 export default channel
